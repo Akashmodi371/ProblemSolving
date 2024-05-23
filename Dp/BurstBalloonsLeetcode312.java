cfc552e4e989a8a991115e58d0b68786e5b45b9a
Akashmodi937 <123933571+Akashmodi937@users.noreply.github.com>
2024-05-23 20:24:27 +0530

    adding microsoft oa sd1

---
+++ Dp/BurstBalloonsLeetcode312.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class BurstBalloonsLeetcode312 {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        int n=sc.nextInt();
+
+        int[] arr=new int[n+1];
+
+        int i=1;
+        while(i<=n){
+            arr[i]=sc.nextInt();
+            i++;
+        }
+
+        
+
+
+
+
+
+
+
+    }
+}
