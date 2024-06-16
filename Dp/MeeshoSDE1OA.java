e10951fdc4d2d1f776b2eb18d5326a6c5ff6583d
Akashmodi937 <123933571+Akashmodi937@users.noreply.github.com>
2024-06-16 16:51:17 +0530

    Adding meesho SDE1

---
+++ Dp/MeeshoSDE1OA.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class MeeshoSDE1OA {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        int n=sc.nextInt();
+        int[] arr=new int[n+1];
+
+        int i=0;
+        while(i<n){
+            arr[i]=sc.nextInt();
+            i++;
+        }
+
+        
+    }
+}
